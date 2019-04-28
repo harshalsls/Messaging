d81ed9979c117878495a133ee9be5fe80409993a
harshal <harshalsls41069@gmail.com>
2019-04-28 12:31:39 +0530

    changed package name

--- src/main/java/com/gibsons/messagingapp/MainApplication.java
+++ src/main/java/com/gibsons/messagingapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.gibsons.MessagingApp;
+package com.gibsons.messagingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
@@ -8,6 +8,7 @@ public class MainApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		SpringApplication.run(MainApplication.class, args);
 	}
 
